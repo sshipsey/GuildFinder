a189131afe5400854e5e56c91578d771ec692fa0
Steven Shipsey <Steven Shipsey>
2016-08-23 00:10:42 -0400

    Cleaned up a bit.

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ def fetchpage1(params):
 # Fetch a subsequent page of guild list from URL
 def fetchnextpage(p):
     return requests.get(baseURL + '/rating/next/' + str(p)).content
+
+# Create a Soup that takes an HTML object as a parameter and returns
+# al list of Tag objects meeting our criteria
+def CreateSoup(v):
+    return BeautifulSoup(v, parser).find_all("a", {"class":"guild horde"})
     
 # Returns a list of guild names
 def getguilds(u, n):
@@ -25,11 +30,11 @@ def getguilds(u, n):
 
 # Get first page of guild names
 def getpage1(url):
-    return (tag.nobr.string for tag in BeautifulSoup(fetchpage1(params), parser).find_all("a", {"class":"guild horde"}))
+    return (tag.nobr.string for tag in CreateSoup(fetchpage1(params)))
 
 # Get subsequent pages as a generator
 def getnextpages(url, numPages):
-    return chain.from_iterable((tag.nobr.string for tag in BeautifulSoup(fetchnextpage(page),parser).find_all("a", {"class":"guild horde"})) for page in range(0, numPages - 1))
+    return chain.from_iterable((tag.nobr.string for tag in CreateSoup(fetchnextpage(page))) for page in range(0, numPages - 1))
     
 # Function call
 for g in getguilds(baseURL, numPages): print(g)
