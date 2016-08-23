effcefb450993d9e5ae12698e8d2b9c89c649637
Steven Shipsey <Steven Shipsey>
2016-08-23 00:03:20 -0400

    Refactored to use BeautifulSoup. Needs cleanup.

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from lxml import html, etree
 import requests
 from itertools import chain
+from bs4 import BeautifulSoup
 
 server = 'thrall'
 numGuilds = 100
-xpath = '//a[@class="guild horde"]/nobr'
+params = '?faction=horde'
+parser = "lxml"
 
 baseURL = 'http://www.wowprogress.com/pve/us/' + server
 numPages = int(numGuilds / 20) + 1
 
+# Fetch page 1 of guild list from URL
+def fetchpage1(params):
+    return requests.get(baseURL + params).content
+
+# Fetch a subsequent page of guild list from URL
+def fetchnextpage(p):
+    return requests.get(baseURL + '/rating/next/' + str(p)).content
+    
 # Returns a list of guild names
 def getguilds(u, n):
     # Get page 1 (different URL parameters) and chain it to the subsequent pages
@@ -16,11 +25,11 @@ def getguilds(u, n):
 
 # Get first page of guild names
 def getpage1(url):
-    return (child.text_content() for child in html.fromstring(requests.get(url + '?faction=horde&raids_week=&lang=en&class=&spec=').content).xpath(xpath))
+    return (tag.nobr.string for tag in BeautifulSoup(fetchpage1(params), parser).find_all("a", {"class":"guild horde"}))
 
 # Get subsequent pages as a generator
 def getnextpages(url, numPages):
-    return chain.from_iterable((child.text_content() for child in html.fromstring(requests.get(baseURL + '/rating/next/' + str(page) + '/rating./raids_week./faction.horde/lang./class./spec.#rating290').content).xpath(xpath)) for page in range(0,numPages - 1))
-
+    return chain.from_iterable((tag.nobr.string for tag in BeautifulSoup(fetchnextpage(page),parser).find_all("a", {"class":"guild horde"})) for page in range(0, numPages - 1))
+    
 # Function call
 for g in getguilds(baseURL, numPages): print(g)
